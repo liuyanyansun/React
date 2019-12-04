ea02daa80091265b5cd0b6e53c46f2f864b0b5b9
liuyanyansun <1694614518@qq.com>
2019-12-04 22:36:29 +0800

    message

---
+++ precache-manifest.3b93c1c154a72120379a89768aef2568.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f77105a3c4286680304aaf8bd827bc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af29c848abc724166958",
+    "url": "/static/css/2.59c644fd.chunk.css"
+  },
+  {
+    "revision": "1dc8cd9b1c4a6be199f0",
+    "url": "/static/css/main.68557faa.chunk.css"
+  },
+  {
+    "revision": "af29c848abc724166958",
+    "url": "/static/js/2.aa491650.chunk.js"
+  },
+  {
+    "revision": "1dc8cd9b1c4a6be199f0",
+    "url": "/static/js/main.14dc3dff.chunk.js"
+  },
+  {
+    "revision": "628ea25a63cbc5b89a27",
+    "url": "/static/js/runtime-main.9f147cbc.js"
+  },
+  {
+    "revision": "6d04b8794c1726337e6f2b872cb882cd",
+    "url": "/static/media/2.6d04b879.jpg"
+  },
+  {
+    "revision": "d672ff82a4cf7c886ca4eadcd11bc9d2",
+    "url": "/static/media/xx.d672ff82.jpg"
+  }
+]);
